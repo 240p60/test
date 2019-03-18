849e26480b523d380953ef6a0d097f2c216c0168
Booker Bryen <bbryen@e6r9p2.21-school.ru>
2019-03-18 23:05:12 +0300

    point

---
+++ ft_point/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbryen <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/18 22:53:54 by bbryen            #+#    #+#             */
+/*   Updated: 2019/03/18 23:04:13 by bbryen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "ft_point.h"
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int		main(void)
+{
+	t_point		point;
+
+	set_point(&point);
+	printf("%d\n%d\n", point.x, point.y);
+	return (0);
+}
