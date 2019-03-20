85606f0188e466f2f2163741f14e8a70531f8bfb
Booker Bryen <bbryen@e6r9p4.21-school.ru>
2019-03-21 00:40:48 +0300

    hex

---
+++ print_hex/print_hex.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_hex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbryen <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/21 00:05:32 by bbryen            #+#    #+#             */
+/*   Updated: 2019/03/21 00:40:23 by bbryen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+void	print_hex(char *hex, int a, int nb)
+{
+	if (a > 0)
+	{
+		nb = a % 16;
+		a = a / 16;
+		print_hex(hex, a, nb);
+		ft_putchar(hex[nb]);
+	}
+}
+int		main(void)
+{
+	int a = 5156454;
+	char hex[] = "0123456789abcdef";
+	int nb;
+
+	nb = 0;
+	print_hex(hex, a, nb);
+	ft_putchar('\n');
+	return (0);
+}
